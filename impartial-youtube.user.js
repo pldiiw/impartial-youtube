b860e9d68d89ae11b029e13be421696d99853627
PLD <pldiiw@gmail.com>
2016-03-12 12:26:23 +0100

    Improve hiding with light intervals
    The impartial subfunc are now run in an interval for 5s at page load to
    be sure that it hides the views and subscribers counts.

--- impartial-youtube.user.js
+++ impartial-youtube.user.js
@@ -7,26 +7,41 @@
 // ==/UserScript==
 
 'use strict';
-const location = window.location; // for semistandard's sake
-document.onreadystatechange = () => {
-  //if (document.readyState === 'complete') impartial();
-};
+const startTime = Date.now();
+let location = window.location; // for semistandard's sake
 impartial();
+window.setInterval(() => {
+  if (location != window.location) {
+    impartial();
+    location = window.location;
+  }
+}, 100);
 
 function impartial () {
-  if (location.pathname !== '/watch') {
+  if (location.pathname === '/' ||
+      location.pathname === '/feed/subscriptions' ||
+      location.pathname === '/results' ||
+      location.pathname === '/feed/trending' ||
+      location.pathname === '/feed/history') {
     // hide views counts on home page, search page, trending page
     // and subscription page
-    impartialNav();
+    const navInterval = window.setInterval(() => {
+      impartialNav();
+      if (startTime - Date.now() > 5000) clearInterval(navInterval);
+    }, 100);
   }
   else if (location.pathname === '/watch') {
     // tweaks on video page
     impartialWatch();
   }
-  else if (location.pathname.slice(0, 5) === '/user') {
+  else if (location.pathname.slice(0, 5) === '/user' ||
+           location.pathname.slice(0, 8) === '/channel') {
     // hide views counts and subscribers count on user's page, user's videos
     // page and channels page
-    impartialUsers();
+    const usersInterval = window.setInterval(() => {
+      impartialUsers();
+      if (startTime - Date.now() > 5000) clearInterval(usersInterval);
+    }, 100);
   }
 
   // TODO: hide subscribers count when hovering an username
@@ -40,17 +55,13 @@ function impartialNav () {
   Array.prototype.forEach.call(
     document.querySelectorAll('.yt-lockup-meta-info'),
     (v) => {
-      if (
-        location.pathname === '/' ||
-        location.pathname === '/feed/subscriptions'
-      ) {
+      if (location.pathname === '/' ||
+          location.pathname === '/feed/subscriptions') {
         v.childNodes[0].style.display = 'none';
       }
-      else if (
-        location.pathname === '/results' ||
-        location.pathname === '/feed/trending' ||
-        location.pathname === '/feed/history'
-      ) {
+      else if (location.pathname === '/results' ||
+               location.pathname === '/feed/trending' ||
+               location.pathname === '/feed/history') {
         if (v.childNodes.length === 1) {
           v.childNodes[0].style.display = 'none';
         } else {
@@ -113,21 +124,24 @@ function impartialWatchComments () {
 }
 
 function impartialUsers () {
-  document.querySelector('.count').style.display = 'none';
+  Array.prototype.forEach.call(
+    document.querySelectorAll('.view-count'),
+    (v) => v.style.display = 'none');
 
   Array.prototype.forEach.call(
     document.querySelectorAll('.yt-lockup-meta-info'),
     (v) => {
-      if (/\/user\/.*\/videos/.test(location.pathname)) {
-        v.childNodes[0].style.display = 'none';
+      if (/\/(user|channel)\/.*\/videos/.test(location.pathname)) {
+        v.children[0].style.display = 'none';
       } else {
-        v.childNodes[0].style.display = 'none';
+        v.children[1].style.display = 'none';
+        v.children[0].style.display = 'none';
       }
     });
 
   document.querySelector('.subscribed').style.display = 'none';
 
-  if (/\/user\/.*\/channels/.test(location.pathname)) {
+  if (/\/(user|channel)\/.*\/channels/.test(location.pathname)) {
     Array.prototype.forEach.call(
       document.querySelectorAll('.yt-subscription-button-subscriber-count-unbranded-horizontal'),
       (v) => v.style.display = 'none');
