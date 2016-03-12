974e506f57e5c3f4329271fdaf0e7a17e231bce8
PLD <pldiiw@gmail.com>
2016-03-12 08:45:13 +0100

    Add semistandard coding style and es6 let/const
    Formatted code to fit semistandard coding style and added a snazzy
    corresponding badge to the readme. Replaced 'var' declarations with
    'let' and 'const' declarations, because es6.

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # impartial-youtube
+[![js-semistandard-style](https://img.shields.io/badge/code%20style-semistandard-brightgreen.svg?style=flat-square)](https://github.com/Flet/semistandard)
 Focus on the content, damn.
 
 ## Synopsis
 
-**impartial-youtube** is a [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) script that aims at removing from the user's perspective some [Youtube](https://www.youtube.com/) "_features_" that, to [my](https://github.com/pldiiw) point of view, might influence an user's opinion and behaviour.
+**impartial-youtube** is a [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) script that aims at removing from the user's perspective some [Youtube](https://www.youtube.com/) '_features_' that, to [my](https://github.com/pldiiw) point of view, might influence an user's opinion and behaviour.
 
 ## Features
 
@@ -20,7 +21,8 @@ Focus on the content, damn.
 
 ## Motivation
 
-[Youtube](https://www.youtube.com/) is awesome but it happens a lot when surfing on it that your attention get caught by a video because of its seductive, click-baiting title, and, when lowering the eyes on the views count, your most basic instinct make you click on it. Hours, years and life are wasted this way. It may be a bit overstated, yes. Navigating Youtube without this central indicator in the Youtube world helps you focus on the content, and, build your own opinion on a video before being influenced by the others, which is generaly more sane and makes the world go forward.  
+[Youtube](https://www.youtube.com/) is awesome but it happens a lot when surfing on it that your attention get caught by a video because of its seductive, click-baiting title, and, when lowering the eyes on the views count, your most basic instinct make you click on it.  
+Hours, years and life are wasted this way. It may be a bit overstated, yes. Navigating Youtube without this central indicator in the Youtube world helps you focus on the content, and, build your own opinion on a video before being influenced by the others, which is generaly more sane and makes the world go forward.  
 Speaking of others, the only rallying thing that is left with this script are the comments. Mainly, confess it, not idealy used by most people, they can be a source of spoils and, as stated earlier, early influences. But, for some videos, it may be necessary for you to read some comments or participate in a discussion. To get everyone to agree, thanks to this script, the comments are only displayed if you want to.
 
 ## Installation
--- impartial-youtube.user.js
+++ impartial-youtube.user.js
@@ -6,61 +6,66 @@
 // @grant       none
 // ==/UserScript==
 
-var location = window.location; // for semistandard's sake
+'use strict';
+
+const location = window.location; // for semistandard's sake
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') window.setInterval(impartial, 1000);
 };
 
 function impartial () {
-  // hide views counts on home page, search page, trending page and subscription page
+  // hide views counts on home page, search page, trending page
+  // and subscription page
   if (location.pathname !== '/watch') {
-    Array.prototype.forEach.call(document.querySelectorAll('.yt-lockup-meta-info'), (v) => {
-      if (
-        location.pathname === '/' ||
-        location.pathname === '/feed/subscriptions'
-      ) {
-        v.childNodes[0].style.display = 'none';
-      }
-      else if (
-        location.pathname === '/results' ||
-        location.pathname === '/feed/trending' ||
-        location.pathname === '/feed/history'
-      ) {
-        if (v.childNodes.length === 1) {
+    Array.prototype.forEach.call(
+      document.querySelectorAll('.yt-lockup-meta-info'),
+      (v) => {
+        if (
+          location.pathname === '/' ||
+          location.pathname === '/feed/subscriptions'
+        ) {
           v.childNodes[0].style.display = 'none';
-        } else {
-          v.childNodes[1].style.display = 'none'; 
         }
-      }
-    });
+        else if (
+          location.pathname === '/results' ||
+          location.pathname === '/feed/trending' ||
+          location.pathname === '/feed/history'
+        ) {
+          if (v.childNodes.length === 1) {
+            v.childNodes[0].style.display = 'none';
+          } else {
+            v.childNodes[1].style.display = 'none';
+          }
+        }
+      });
   }
   // tweaks on video page
   else if (location.pathname === '/watch') {
     // hide views counts in recommendations
-    Array.prototype.forEach.call(document.querySelectorAll('.view-count'), (v) => {
-      v.style.display = 'none';
-    });
-    
+    Array.prototype.forEach.call(
+      document.querySelectorAll('.view-count'),
+      (v) => v.style.display = 'none');
+
     // hide video's views and likes/dislikes bar and count
     document.querySelector('#watch8-sentiment-actions').style.display = 'none';
-    
+
     // hide video's uploader's subscribers count
     document.querySelector('.yt-subscriber-count').style.display = 'none';
-    
+
     // hide comments and count
-    var commentsHeader = document.querySelector('h2.comment-section-header-renderer');
-    var sortMenu = document.querySelector('.comment-section-sort-menu');
-    var comments = document.querySelector('#comment-section-renderer-items');
-    var loadMoreBtn = document.querySelector('.comment-section-renderer-paginator');
-    
-    var commentsHeaderOriginalInnerHTML = commentsHeader.innerHTML;
+    let commentsHeader = document.querySelector('h2.comment-section-header-renderer');
+    let sortMenu = document.querySelector('.comment-section-sort-menu');
+    let comments = document.querySelector('#comment-section-renderer-items');
+    let loadMoreBtn = document.querySelector('.comment-section-renderer-paginator');
+
+    const commentsHeaderOriginalInnerHTML = commentsHeader.innerHTML;
     commentsHeader.innerHTML = '<b>Commentaires</b>';
     sortMenu.style.display = 'none';
     comments.style.display = 'none';
     loadMoreBtn.style.display = 'none';
-    
+
     // add a button to display comments
-    var showCommentsBtn = document.createElement('button');
+    let showCommentsBtn = document.createElement('button');
     showCommentsBtn.innerHTML = 'Show comments';
     showCommentsBtn.className = loadMoreBtn.className;
     showCommentsBtn.style.display = 'block';
@@ -74,28 +79,30 @@ function impartial () {
     document.querySelector('#comment-section-renderer')
       .insertBefore(showCommentsBtn, loadMoreBtn);
   }
-  // hide views counts and subscribers count on user's page, user's videos page and channels page
+  // hide views counts and subscribers count on user's page, user's videos page
+  // and channels page
   else if (location.pathname.slice(0, 5) === '/user') {
     document.querySelector('.count').style.display = 'none';
-    
-    Array.prototype.forEach.call(document.querySelectorAll('.yt-lockup-meta-info'), (v) => {
-      if (/\/user\/.*\/videos/.test(location.pathname)) {
-        v.childNodes[0].style.display = 'none';
-      } else {
-        v.childNodes[0].style.display = 'none';
-      }
-    });
-    
+
+    Array.prototype.forEach.call(
+      document.querySelectorAll('.yt-lockup-meta-info'),
+      (v) => {
+        if (/\/user\/.*\/videos/.test(location.pathname)) {
+          v.childNodes[0].style.display = 'none';
+        } else {
+          v.childNodes[0].style.display = 'none';
+        }
+      });
+
     document.querySelector('.subscribed').style.display = 'none';
-    
+
     if (/\/user\/.*\/channels/.test(location.pathname)) {
       Array.prototype.forEach.call(
         document.querySelectorAll('.yt-subscription-button-subscriber-count-unbranded-horizontal'),
-        (v) => v.style.display = 'none'
-      );
+        (v) => v.style.display = 'none');
     }
   }
-  
+
   // TODO: hide subscribers count when hovering an username
   Array.prototype.forEach.call(
     document.querySelectorAll('.subscribed'),
