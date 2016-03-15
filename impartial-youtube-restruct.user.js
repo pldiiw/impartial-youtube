da4ee520b20fb87213b7950eec6a23a609a4cdfd
PLD <pldiiw@gmail.com>
2016-03-15 01:23:14 +0100

    Modify show comments button
    Modify show comments button to make it appears at the loading of the
    page instead of an awkward interval check to create it.
    
    Doing it this way is more stable, the only downside is that the user
    won't be able to comment the video without pushing the show comments
    button.

--- impartial-youtube-restruct.user.js
+++ impartial-youtube-restruct.user.js
@@ -12,60 +12,57 @@ impartial();
 
 function impartial () {
   if (window.location.pathname === '/') {
+    // hide views counts on home page
     hideChildAtIndex('.yt-lockup-meta-info', 0);
   } else if (window.location.pathname === '/results') {
+    // hide views counts on result page
     forEachEltWithClass('.yt-lockup-meta-info', v => {
       let i = 1;
-      if (v.children.length === 1) i = 0;
+      if (v.children.length === 1) i = 0; // if it is an ad
       v.children[i].style.display = 'none';
     });
   } else if (window.location.pathname === '/feed/subscriptions') {
+    // hide views counts on subscriptions page
     hideChildAtIndex('.yt-lockup-meta-info', 0);
   } else if (window.location.pathname === '/feed/trending') {
+    // hide views counts on trending page
     hideChildAtIndex('.yt-lockup-meta-info', 1);
   } else if (window.location.pathname === '/feed/history') {
+    // hide views counts on history page
     hideChildAtIndex('.yt-lockup-meta-info', 0);
   } else if (window.location.pathname === '/watch') {
+    // hide video's view count, likes and uploader's subscriber count
     forEachEltWithClass('.view-count', v => v.style.display = 'none');
     document.querySelector('#watch8-sentiment-actions').style.display = 'none';
     document.querySelector('.yt-subscriber-count').style.display = 'none';
 
-    let commentsHeader = document.querySelector('h2.comment-section-header-renderer');
-    let sortMenu = document.querySelector('.comment-section-sort-menu');
-    let comments = document.querySelector('#comment-section-renderer-items');
-    let loadMoreBtn = document.querySelector('.load-more-text')
-      .parentElement
-      .parentElement;
+    // hide video's comments
+    let commentSection = document.querySelector('#comment-section-renderer');
+    commentSection.style.display = 'none';
 
-    const commentsHeaderOriginalInnerHTML = commentsHeader.innerHTML;
-    commentsHeader.innerHTML = '<b>Commentaires</b>';
-    sortMenu.style.display = 'none';
-    comments.style.display = 'none';
-    loadMoreBtn.style.display = 'none';
-
+    // create a 'Show comments' button
     let showCommentsBtn = document.createElement('button');
     showCommentsBtn.innerHTML = 'Show comments';
     showCommentsBtn.className = 'yt-uix-button yt-uix-button-size-default yt-uix-button-default load-more-button yt-uix-load-more yt-uix-sessionlink comment-section-renderer-paginator';
     showCommentsBtn.style.display = 'block';
     showCommentsBtn.onclick = () => {
-      commentsHeader.innerHTML = commentsHeaderOriginalInnerHTML;
-      sortMenu.style.display = '';
-      comments.style.display = '';
-      loadMoreBtn.style.display = '';
       showCommentsBtn.style.display = 'none';
+      commentSection.style.display = '';
     };
-    document.querySelector('#comment-section-renderer')
-      .insertBefore(showCommentsBtn, loadMoreBtn);
+
+    // display button
+    document.querySelector('#watch-discussion').appendChild(showCommentsBtn);
   } else if (/^\/(user|channel)/.test(window.location.pathname)) {
+    // hide views counts and subscribers count on a user page and user's videos page
     forEachEltWithClass('.view-count', v => v.style.display = 'none');
     hideChildAtIndex('.yt-lockup-meta-info', 0);
     forEachEltWithClass('.subscribed', v => v.style.display = 'none');
 
+    // hide subscribers counts on user's channels page
     if (/channels$/.test(window.location.pathname)) {
       forEachEltWithClass(
         '.yt-subscription-button-subscriber-count-unbranded-horizontal',
-        v => v.style.display = 'none'
-      );
+        v => v.style.display = 'none');
     }
   }
 }
@@ -77,3 +74,33 @@ function forEachEltWithClass (query, fn) {
 function hideChildAtIndex (query, index) {
   forEachEltWithClass(query, v => v.children[index].style.display = 'none');
 }
+
+/*
+// Old button
+let commentsHeader = document.querySelector('h2.comment-section-header-renderer');
+let sortMenu = document.querySelector('.comment-section-sort-menu');
+let comments = document.querySelector('#comment-section-renderer-items');
+let loadMoreBtn = document.querySelector('.load-more-text')
+  .parentElement
+  .parentElement;
+
+const commentsHeaderOriginalInnerHTML = commentsHeader.innerHTML;
+commentsHeader.innerHTML = '<b>Commentaires</b>';
+sortMenu.style.display = 'none';
+comments.style.display = 'none';
+loadMoreBtn.style.display = 'none';
+
+let showCommentsBtn = document.createElement('button');
+showCommentsBtn.innerHTML = 'Show comments';
+showCommentsBtn.className = 'yt-uix-button yt-uix-button-size-default yt-uix-button-default load-more-button yt-uix-load-more yt-uix-sessionlink comment-section-renderer-paginator';
+showCommentsBtn.style.display = 'block';
+showCommentsBtn.onclick = () => {
+  commentsHeader.innerHTML = commentsHeaderOriginalInnerHTML;
+  sortMenu.style.display = '';
+  comments.style.display = '';
+  loadMoreBtn.style.display = '';
+  showCommentsBtn.style.display = 'none';
+};
+document.querySelector('#comment-section-renderer')
+  .insertBefore(showCommentsBtn, loadMoreBtn);
+*/
